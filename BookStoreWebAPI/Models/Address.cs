d305990ad19146e3817d2c6cebd5394b9555c6e3
fkoca <ferdikoca12@gmail.com>
2024-01-25 13:59:55 +0300

    add address to user

--- BookStoreWebAPI/Models/Address.cs
+++ BookStoreWebAPI/Models/Address.cs
@@ -10,5 +10,6 @@
 
         public int UserId { get; set; }
         public User? User { get; set; }
+        public List<Order>? Orders { get; set; }
     }
 }
--- BookStoreWebAPI/Models/Order.cs
+++ BookStoreWebAPI/Models/Order.cs
@@ -9,5 +9,7 @@
         public int ProductId { get; set; }
         public Product? Product { get; set; }
         public int Quantity { get; set; }
+        public int AddressId { get; set; }
+        public Address? Address { get; set; }
     }
 }
